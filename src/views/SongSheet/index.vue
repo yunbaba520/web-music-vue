ce274867ea276f7703842d948bef97d94e1afc95
zhazhalong <1650620783@qq.com>
2022-03-20 20:20:38 +0800

    歌单view初始化，新建分支songsheet

--- src/views/SongSheet/index.vue
+++ src/views/SongSheet/index.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
 <h2>歌单</h2>
+<song-sheet-options/>
+<song-sheet-list/>
     </div>
 </template>
 
 <script>
+import SongSheetOptions from './c_cpns/songsheet-options.vue'
+import SongSheetList from './c_cpns/songsheet-list.vue'
     export default {
-        
+        components:{
+            SongSheetOptions,
+            SongSheetList
+        }
     }
 </script>
 
